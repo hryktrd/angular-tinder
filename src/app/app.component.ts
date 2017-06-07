2616b70e9ec2fbfd68101ce677856ea451bf05d8
Hiroyuki Terada <hissan1983@gmail.com>
2017-06-07 16:59:39 +0900

    左右スワイプ実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    onSwipeleft(idx, event) {
+        console.log(event);
+    }
+    onSwiperight(idx, event) {
+        console.log(event);
+    }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {HammerGestureConfig, HAMMER_GESTURE_CONFIG, BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import {AppComponent} from './app.component';
+import 'hammerjs/hammer';
+
+export class MyHammerConfig extends HammerGestureConfig {
+    overrides = <any>{
+        'swipe': {velocity: 0.4, threshold: 20} // override default settings
+    };
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [{
+        provide: HAMMER_GESTURE_CONFIG,
+        useClass: MyHammerConfig
+    }],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
